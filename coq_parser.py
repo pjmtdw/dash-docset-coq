a215b83ea5b8e47155244ffc3d54e42ad842d416
Haruhiro Yoshimoto <pjmtdw@gmail.com>
2018-12-14 11:40:19 +0900

    fix link

--- coq_parser.py
+++ coq_parser.py
@@ -2,7 +2,7 @@ from doc2dash.parsers.intersphinx import InterSphinxParser
 
 # Sphinx Index Type -> Dash Entry Type
 # Supported Entry Types
-#    Dash: https://kapeli.com/docsets#supportedentrytypes
+#    Dash: https://github.com/Kapeli/Dash-iOS/blob/master/Dash/DHTypes.m
 #          https://github.com/Kapeli/Dash-iOS/tree/master/Dash/Images.xcassets/Types
 #    Zeal: https://github.com/zealdocs/zeal/blob/master/src/app/resources/zeal.qrc
 
